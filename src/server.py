057f9ac1fd728eb4667ca15f2b6efc32ae5c43a3
Aleksander <Aleksanderbo10@gmail.com>
2021-02-27 20:25:31 +0100

    add logo on startup for server

--- src/server.py
+++ src/server.py
@@ -1,21 +1,34 @@
 import socket
+from logo import Logo
+
+class ServerSocket:
+    def __init__(self):
+        self.PORT = 12345
+        self.SIZE = 1024
+        self.HOST = ""
+        self.logo = Logo()
 
 
-PORT = 12345
-SIZE = 1024
-HOST = ""
+    def start(self):
+        self.logo.draw()
+
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.bind((self.HOST, self.PORT))
+        print(f"Server binded to {self.PORT}")
+        self.socket.listen(5)
+        print(f"Server listening to {self.PORT}")
 
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((HOST, PORT))
-print(f"Server binded to {PORT}")
+    def get_info(self):
+        while True:
+            client, address = self.socket.accept()
+            print("Received connection from", address)
+            data = client.recv(self.SIZE)
+            print("Client:", data.decode("utf-8"))
+            client.close()
 
-s.listen(5)
-print(f"Server listening to {PORT}")
 
-while True:
-    client, address = s.accept()
-    print("Received connection from", address)
-    data = client.recv(SIZE)
-    print("Client:", data.decode("utf-8"))
-    client.close()
+if __name__ == "__main__":
+    server = ServerSocket()
+    server.start()
+    server.get_info()
