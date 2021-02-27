d5c31f88a173cf930045bf095dfbcef1f0467670
Aleksander <Aleksadnerbo10@gmail.com>
2021-02-27 17:46:49 +0100

    ip now set to IPv4 instead of localhost

--- src/client.py
+++ src/client.py
@@ -1,13 +1,15 @@
 import socket
-
+import os 
+from dotenv import load_dotenv
 
 class ClientSocket:
     def __init__(self):
         self.PORT = 12345
+        load_dotenv()
 
     def send_message(self, msg):
         mysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         msg = str.encode(msg, "utf-8")
-        mysocket.connect(("127.0.0.1", self.PORT))
+        mysocket.connect((os.getenv("SERVER_IP"), self.PORT))
         mysocket.send(msg)
         mysocket.close()
